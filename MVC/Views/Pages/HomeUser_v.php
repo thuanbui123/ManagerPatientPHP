0a66490131b0e6ea5b4f465d1e0d70f1282e9e61
tannguyen-w <ntantfs1603@gmail.com>
2023-10-16 20:39:57 +0700

    new 2 update user

--- MVC/Views/Pages/HomeUser_v.php
+++ MVC/Views/Pages/HomeUser_v.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Document</title>
-<<<<<<< HEAD
 
 
     <link rel="stylesheet" href="http://localhost/ManagerPatientPHP/Public/css/homeUser.css">
@@ -31,9 +30,6 @@
         console.log(add);
     }
     </script>
-=======
-    <link rel="stylesheet" href="http://localhost/ManagerPatientPHP/Public/css/editdoctor.css">
->>>>>>> b7675c2aea9c1be4cc36790ec6c2a96adb866a10
 </head>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -47,7 +43,6 @@
 </script>
 
 <body>
-<<<<<<< HEAD
 
     <div class="container info">
         <!-- User -->
@@ -307,19 +302,6 @@
     </div>
 
     <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
-=======
-    <div class="main">
-        <header class="header">
-            <div class="page">
-                <h1 class="name__page">Trang chủ</h1>
-                <h3 class="desc __page">Thông tin mới</h3>
-            </div>
-        </header>
-
-        
-
-    </div>
->>>>>>> b7675c2aea9c1be4cc36790ec6c2a96adb866a10
 
 </body>
 
